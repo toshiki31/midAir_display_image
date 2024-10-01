41c90932bdde0a3c3be0123c017304725b86d4eb
toshiki31 <toshiki.m.020131@outlook.jp>
2024-10-01 17:36:44 +0900

    fontsizeを100に変更

--- project/subtitle.py
+++ project/subtitle.py
@@ -63,7 +63,7 @@ canvas.pack()
 canvas.create_image(0, 0, image=photo, anchor=tk.NW)
 
 # テキストを画像内に表示する
-text_id = canvas.create_text(popup_width / 2, 80, text="ここに文字が表示されます", font=("Arial", 80, "bold"), fill="black")
+text_id = canvas.create_text(popup_width / 2, 80, text="ここに文字が表示されます", font=("Arial", 100, "bold"), fill="black")
 
 # テキスト入力フィールド (改行を許可)
 entry = tk.Text(root, width=30, height=5)
